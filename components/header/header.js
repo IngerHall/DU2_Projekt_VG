3c506caa943a55694b92b5707d27f63f13ba0430
Johanna <johanna.hallinger99@gmail.com>
2024-01-07 16:53:46 +0100

    added sort and cart to header

--- components/header/header.js
+++ components/header/header.js
@@ -4,51 +4,61 @@
 function render_header_structure() {
   const header_container = document.querySelector("header");
   header_container.innerHTML = `<h1>LOONEY LACES</h1>
-
+                                
                                 <div id="filter">
-                                  <div class="button">
+                                  <div>
                                     <div id="filter_title">FILTER</div>
-                                    <div class="arrow" id="arrow_one">V</div>
                                   </div>
-                                  <div class="dropdown_menu hide">
+                                  <div class="dropdown_filter hide">
+                                  </div>
+                                </div>
+
+                                <div id="sort">
+                                  <div>
+                                    <div id="sort_title">SORT</div>
+                                  </div>
+                                  <div class="dropdown_sort hide">
                                   </div>
                                 </div>
 
                                 <div id="about_us">
-                                  <div class="button">
-                                    <div id="about_us_title">OM OSS</div>
-                                    <div class="arrow" id="arrow_two">V</div>
+                                  <div>
+                                    <div id="about_us_title">ABOUT US</div>
                                   </div>
                                   <div class="about_us_box hide">
                                   </div>
+                                </div>
+                                
+                                <div id="cart">
+                                  <div class="cart">
+                                    <div id="cart_title">CART</div>
+                                  </div>
                                 </div>`;
 
-  // Följande kod ser till att när man trycker på texten "FILTER" eller pilen bredvid,
-  // så kommer dropdown menyn att visas/gömmas
+  // Filter
+  
   const filter_title = document.querySelector("#filter_title");
-  const arrow_one = document.querySelector("#arrow_one");
-  const dropdown_menu = document.querySelector(".dropdown_menu");
+  const dropdown_filter = document.querySelector(".dropdown_filter");
 
   filter_title.addEventListener("click", function () {
-    dropdown_menu.classList.toggle("hide");
+    dropdown_filter.classList.toggle("hide");
   });
 
-  arrow_one.addEventListener("click", function () {
-    dropdown_menu.classList.toggle("hide");
-  });
+  // Sort
 
-  // Följande kod ser till att när man trycker på texten "OM OSS" eller pilen bredvid,
-  // så kommer about us boxen att visas/gömmas
+  const sort_title = document.querySelector("#sort_title");
+  const dropdown_sort = document.querySelector(".dropdown_sort")
+  
+  sort_title.addEventListener("click", function () {
+    dropdown_sort.classList.toggle("hide");
+  })
+
+  // About us
 
   const about_us_title = document.querySelector("#about_us_title");
-  const arrow_two = document.querySelector("#arrow_two");
   const about_us_box = document.querySelector(".about_us_box");
 
   about_us_title.addEventListener("click", function () {
-    about_us_box.classList.toggle("hide");
-  });
-
-  arrow_two.addEventListener("click", function () {
     about_us_box.classList.toggle("hide");
   });
 }
--- index.js
+++ index.js
@@ -2,9 +2,9 @@ render_div_container(document.body);
 
 render_header_structure();
 
-const dropdown_menu = document.querySelector(".dropdown_menu");
-render_type_filters(dropdown_menu);
-render_country_filters(dropdown_menu);
-render_price_filters(dropdown_menu);
+const dropdown_filter = document.querySelector(".dropdown_filter");
+render_type_filters(dropdown_filter);
+render_country_filters(dropdown_filter);
+render_price_filters(dropdown_filter);
 
 render_bottom_container(filter_programmes());
